c9ae7b02181972d68d2832f13f6d5144a905e4a2
Robby Abaya <robby@getpinwheel.com>
2021-06-30 13:53:35 -0400

    Add callbacks to MainActivity, and capture events/

--- app/src/main/java/com/example/byowebview/MainActivity.kt
+++ app/src/main/java/com/example/byowebview/MainActivity.kt
@@ -7,10 +7,27 @@ import com.underdog_tech.pinwheel_android.Pinwheel
 import com.underdog_tech.pinwheel_android.PinwheelEventListener
 import com.underdog_tech.pinwheel_android.model.*
 
-class Listener : PinwheelEventListener {
+data class CapturedEvent(val eventName: PinwheelEventType, var payload: PinwheelEventPayload?)
+
+class MainActivity : AppCompatActivity(), PinwheelEventListener {
+    val capturedEvents = mutableListOf<CapturedEvent>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pinwheel_fragment)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val webView = findViewById<WebView>(R.id.webView)
+        val token = "your token from https://developer.getpinwheel.com/test-console"
+        Pinwheel.init(webView, token, this)
+    }
+
     override fun onEvent(eventName: PinwheelEventType, payload: PinwheelEventPayload?) {
         print("onEvent")
         print(eventName.toString())
+        capturedEvents.add(CapturedEvent(eventName, payload))
     }
 
     override fun onError(error: PinwheelError) {
@@ -31,20 +48,5 @@ class Listener : PinwheelEventListener {
     override fun onSuccess(result: PinwheelResult) {
         print("onSuccess")
         print(result.toString())
-    }
-}
-
-class MainActivity : AppCompatActivity() {
-    val listener = Listener()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.pinwheel_fragment)
-    }
-
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        val webView = findViewById<WebView>(R.id.webView)
-        val token = "your token from https://developer.getpinwheel.com/test-console"
-        Pinwheel.init(webView, token, null)
     }
 }
