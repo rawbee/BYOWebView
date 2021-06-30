764a40c35802acc68faf06a8f97911bfb34c5fed
Robby Abaya <robby@getpinwheel.com>
2021-06-30 13:39:17 -0400

    Added event listener to the example

--- app/src/main/java/com/example/byowebview/MainActivity.kt
+++ app/src/main/java/com/example/byowebview/MainActivity.kt
@@ -4,8 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import com.underdog_tech.pinwheel_android.Pinwheel
+import com.underdog_tech.pinwheel_android.PinwheelEventListener
+import com.underdog_tech.pinwheel_android.model.*
+
+class Listener : PinwheelEventListener {
+    override fun onEvent(eventName: PinwheelEventType, payload: PinwheelEventPayload?) {
+        print("onEvent")
+        print(eventName.toString())
+    }
+
+    override fun onError(error: PinwheelError) {
+        print("onError")
+        print(error.toString())
+    }
+
+    override fun onExit(error: PinwheelError?) {
+        print("onExit")
+        print(error.toString())
+    }
+
+    override fun onLogin(result: PinwheelLoginPayload) {
+        print("onLogin")
+        print(result.toString())
+    }
+
+    override fun onSuccess(result: PinwheelResult) {
+        print("onSuccess")
+        print(result.toString())
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    val listener = Listener()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pinwheel_fragment)
